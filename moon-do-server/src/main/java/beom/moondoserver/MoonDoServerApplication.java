d1f37d2bae3a1d6c1d9bb76575390297cadfbab4
Better Jeong <eunjeong42na@gmail.com>
2023-07-20 16:25:51 +0900

    스프링부트

---
+++ moon-do-server/src/main/java/beom/moondoserver/MoonDoServerApplication.java
@@ -0,0 +1,13 @@
+package beom.moondoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonDoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonDoServerApplication.class, args);
+    }
+
+}
---
+++ moon-do-server/src/test/java/beom/moondoserver/MoonDoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package beom.moondoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonDoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
