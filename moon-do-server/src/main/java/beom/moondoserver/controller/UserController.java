ff1b6bffae9ccff54b3e45431469b7f61788bf73
Better Jeong <eunjeong42na@gmail.com>
2023-08-14 03:22:36 +0900

    프로필 API

--- moon-do-server/src/main/java/beom/moondoserver/controller/UserController.java
+++ moon-do-server/src/main/java/beom/moondoserver/controller/UserController.java
@@ -1,12 +1,22 @@
 package beom.moondoserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import beom.moondoserver.model.dto.response.UserInfoResponse;
+import beom.moondoserver.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/info")
+    public UserInfoResponse getUserInfo(@RequestParam("id") String userId) {
+        return userService.getUserInfo(userId);
+    }
 
 }
