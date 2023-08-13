ad456b0cdba16b50be55e74bb2fa2b01b66650ec
Better Jeong <eunjeong42na@gmail.com>
2023-08-14 02:23:59 +0900

    에러 수정

--- moon-do-server/src/main/java/beom/moondoserver/controller/ProblemController.java
+++ moon-do-server/src/main/java/beom/moondoserver/controller/ProblemController.java
@@ -3,7 +3,11 @@ package beom.moondoserver.controller;
 import beom.moondoserver.model.dto.request.CreateProblemRequest;
 import beom.moondoserver.model.dto.response.CreateProblemResponse;
 import beom.moondoserver.service.ProblemService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -13,7 +17,8 @@ public class ProblemController {
     // 문제 만들기, 문제 저장
     final ProblemService problemService;
 
-    public ProblemController(ProblemService problemService){
+    @Autowired
+    public ProblemController(ProblemService problemService) {
         this.problemService = problemService;
     }
 
