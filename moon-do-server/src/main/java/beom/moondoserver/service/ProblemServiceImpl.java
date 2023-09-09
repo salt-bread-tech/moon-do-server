6c76fabb515c7ef7f1c59901ffe34c6392a14fbc
GiCheols <ngc4665@naver.com>
2023-09-09 20:37:30 +0900

    FEAT: 문제 파싱 메서드 추가
    createProblem() 메서드를 통해 만들어진 문제를 파싱하여 문제만을 추출해 List<String> 형태로 반환하는 메서드를 추가하였습니다.

--- moon-do-server/src/main/java/beom/moondoserver/service/ProblemServiceImpl.java
+++ moon-do-server/src/main/java/beom/moondoserver/service/ProblemServiceImpl.java
@@ -3,15 +3,14 @@ package beom.moondoserver.service;
 import beom.moondoserver.model.dto.request.CreateProblemRequest;
 import beom.moondoserver.model.dto.request.GetProblemRequest;
 import beom.moondoserver.model.dto.request.GetSolutionRequest;
-import beom.moondoserver.model.dto.response.ChatGPTResponse;
-import beom.moondoserver.model.dto.response.CreateProblemResponse;
-import beom.moondoserver.model.dto.response.GetProblemResponse;
-import beom.moondoserver.model.dto.response.GetSolutionResponse;
+import beom.moondoserver.model.dto.response.*;
 import beom.moondoserver.util.GPTManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +25,7 @@ public class ProblemServiceImpl implements ProblemService {
                 + request.getCount() + "개 만큼 출제해줘.";
 
         ChatGPTResponse chatGPTResponse = gptManager.getProblem(prompt);
-        System.out.println(chatGPTResponse);
+        parseProblem(chatGPTResponse);
 
         return insertProblem(chatGPTResponse);
     }
@@ -44,5 +43,27 @@ public class ProblemServiceImpl implements ProblemService {
         // response 를 파싱해서 DB에 넣는 과정 필요
         
         return true;
+    }
+
+    private List<String> parseProblem(ChatGPTResponse chatGPTResponse){
+        String string = chatGPTResponse.getChoices().get(0).getMessage().getContent();
+        StringTokenizer stringTokenizer = new StringTokenizer(string, "\n");
+        List<String> problemResponseList = new ArrayList<>();
+
+        while(stringTokenizer.hasMoreTokens()){
+            if(stringTokenizer.countTokens() % 2 == 1){
+                problemResponseList.add(stringTokenizer.nextToken());
+            }
+            else {
+                stringTokenizer.nextToken();
+            }
+        }
+
+        // 데이터가 잘 파싱되었는지 확인하는 부분, 작업 이후 삭제 예정
+        for (String problem:problemResponseList) {
+            System.out.println(problem);
+        }
+
+        return problemResponseList;
     }
 }
