8170451e3b13fa58b325c53cf0401bc6bcd732ed
Better Jeong <eunjeong42na@gmail.com>
2023-09-16 23:24:53 +0900

    FEAT: 회원 가입 구현
    POST 방식으로 email, password, nickname 전달 시 회원 가입을 처리하는 api 작성

--- moon-do-server/src/main/java/beom/moondoserver/service/UserService.java
+++ moon-do-server/src/main/java/beom/moondoserver/service/UserService.java
@@ -1,7 +1,13 @@
 package beom.moondoserver.service;
 
+import beom.moondoserver.model.dto.request.LoginRequest;
+import beom.moondoserver.model.dto.request.RegisterRequest;
 import beom.moondoserver.model.dto.response.UserInfoResponse;
 
 public interface UserService {
-    UserInfoResponse getUserInfo(String userId);
+    UserInfoResponse getUserInfo(String email);
+
+    String register(RegisterRequest request);
+
+    String login(LoginRequest request);
 }
--- moon-do-server/src/main/java/beom/moondoserver/service/UserServiceImpl.java
+++ moon-do-server/src/main/java/beom/moondoserver/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package beom.moondoserver.service;
 
+import beom.moondoserver.model.dto.request.LoginRequest;
+import beom.moondoserver.model.dto.request.RegisterRequest;
 import beom.moondoserver.model.dto.response.UserInfoResponse;
 import beom.moondoserver.model.entity.User;
 import beom.moondoserver.repository.UserRepo;
@@ -14,8 +16,36 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepo userRepo;
 
-    public UserInfoResponse getUserInfo(String userId) {
-        Optional<User> optionalUser = userRepo.findById(userId);
+    @Override
+    public String register(RegisterRequest request) {
+        String result;
+
+        if (!isExistingId(request.getEmail())) {
+            userRepo.save(User.builder()
+                    .email(request.getEmail())
+                    .password(request.getPassword())
+                    .nickname(request.getNickname())
+                    .introduction("")
+                    .build());
+
+            System.out.println("회원 가입 성공");
+            result = "1";
+        }
+        else {
+            System.out.println("회원 가입 실패: 아이디 중복");
+            result = "2";
+        }
+
+        return result;
+    }
+
+    @Override
+    public String login(LoginRequest request) {
+        return null;
+    }
+
+    public UserInfoResponse getUserInfo(String email) {
+        Optional<User> optionalUser = userRepo.findByEmail(email);
         UserInfoResponse userInfoResponse = new UserInfoResponse();
 
         if (optionalUser.isPresent()) {
@@ -29,6 +59,15 @@ public class UserServiceImpl implements UserService {
         }
 
         return userInfoResponse;
+    }
+
+    private boolean isExistingId(String email) {   // 존재하는 email 인지 검사 (중복 여부)  true: 존재함  false: 존재하지 않음
+        boolean result = false;
+        Optional<User> user = userRepo.findByEmail(email);
+
+        if (user.isPresent()) result = true;
+
+        return result;
     }
 
 }
