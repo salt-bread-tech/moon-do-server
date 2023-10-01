9c298aaa3b3dafc7961b097b26f49a6816fa4872
Better Jeong <eunjeong42na@gmail.com>
2023-10-01 21:37:37 +0900

    FEAT: 문제 생성 프롬프트 수정
    - GPT 요청 시 assistant 를 활용하여 질문하도록 수정
    - 문제 파싱 방식 수정

--- moon-do-server/src/main/java/beom/moondoserver/repository/ProblemPaperRepo.java
+++ moon-do-server/src/main/java/beom/moondoserver/repository/ProblemPaperRepo.java
@@ -1,13 +1,9 @@
 package beom.moondoserver.repository;
 
 import beom.moondoserver.model.entity.ProblemPaper;
-import beom.moondoserver.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProblemPaperRepo extends JpaRepository<ProblemPaper, Integer> {
-    List<ProblemPaper> findAllByUserUserId(Integer userId);
-    // Optional<ProblemPaper> findByProblemPaperId(Integer id);
 }
