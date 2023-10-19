cf4a2492fb00b3c75758094f4a411b79a11766c1
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-10-19 15:26:33 +0900

    FEAT: 북마크 문제지 목록 API 추가
    유저 ID 로 북마크한 문제지 목록 정보를 출력하는 API 추가

--- moon-do-server/src/main/java/beom/moondoserver/controller/ProblemPaperController.java
+++ moon-do-server/src/main/java/beom/moondoserver/controller/ProblemPaperController.java
@@ -1,6 +1,8 @@
 package beom.moondoserver.controller;
 
+import beom.moondoserver.model.dto.request.BookmarkedRequest;
 import beom.moondoserver.model.dto.request.GetInfoRequest;
+import beom.moondoserver.model.dto.response.BookmarkedResponse;
 import beom.moondoserver.model.dto.response.GetInfoResponse;
 import beom.moondoserver.service.ProblemPaperService;
 import beom.moondoserver.service.ProblemPaperServiceImpl;
@@ -26,5 +28,10 @@ public class ProblemPaperController {
     @PostMapping("/info")
     public List<GetInfoResponse> getProblemPaper(@RequestBody GetInfoRequest request){
         return problemPaperService.getInfo(request);
+    }
+
+    @PostMapping("/bookmark")
+    public List<BookmarkedResponse> getBookmarkedProblemPaper(@RequestBody BookmarkedRequest request) {
+        return problemPaperService.getBookmarkedProblemPaper(request);
     }
 }
--- moon-do-server/src/main/java/beom/moondoserver/service/ProblemPaperService.java
+++ moon-do-server/src/main/java/beom/moondoserver/service/ProblemPaperService.java
@@ -1,10 +1,14 @@
 package beom.moondoserver.service;
 
+import beom.moondoserver.model.dto.request.BookmarkedRequest;
 import beom.moondoserver.model.dto.request.GetInfoRequest;
+import beom.moondoserver.model.dto.response.BookmarkedResponse;
 import beom.moondoserver.model.dto.response.GetInfoResponse;
 
 import java.util.List;
 
 public interface ProblemPaperService {
     List<GetInfoResponse> getInfo(GetInfoRequest request);
+
+    List<BookmarkedResponse> getBookmarkedProblemPaper(BookmarkedRequest request);
 }
