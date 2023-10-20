001f6e9cfd12030f8ed5da4d68d45466a2020779
GiCheols <ngc4665@naver.com>
2023-10-21 02:02:58 +0900

    FEAT: Enum 관련 API 수정
    난이도를 나타내는 Enum값에 대한 요청을 보낼 때 각 Enum에 지정된 값(1~5)이 아닌 인덱스(0~4)값을 주어야 했던 오류 수정

--- moon-do-server/src/main/java/beom/moondoserver/util/DifficultyConverter.java
+++ moon-do-server/src/main/java/beom/moondoserver/util/DifficultyConverter.java
@@ -10,15 +10,20 @@ public class DifficultyConverter implements AttributeConverter<Difficulty, Integ
     public Integer convertToDatabaseColumn(Difficulty attribute) {
         if(attribute == null)
             return 0;
-        return attribute.getDifficulty();
+        return attribute.getValue();
     }
 
     @Override
     public Difficulty convertToEntityAttribute(Integer dbData) {
+        if (dbData == null)
+            return null;
+        return Difficulty.ofValue(dbData);
+    /*
         for (Difficulty d : Difficulty.values()) {
-            if (d.getDifficulty() == dbData)
+            if (d.getValue() == dbData)
                 return d;
         }
         throw new IllegalArgumentException("Invalid database value for Difficulty: " + dbData);
+    */
     }
 }
